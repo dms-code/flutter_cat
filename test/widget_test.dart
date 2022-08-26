1c97bba96bd842ec82519e394a7208b9d855c512
Douglas Souza <cdscasd>
2022-08-26 15:18:33 -0300

    Create the folder structure for an MVP (Model-View-Presenter) architecture with repository pattern support.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_cat/main.dart';
+import 'package:flutter_cat/ui/view/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
